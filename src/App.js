4a3ab553394dd0f0e3815999ac5f043599abde65
itsmetechy@gmail.com <sivashankar.kuppusamy@keypathedu.com>
2020-03-17 15:20:29 +0530

    Corona first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Container, Row, Col, Form, Alert } from 'react-bootstrap'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="app-header">
+        {
+          <h3>{'Covid19 Update'}</h3>
+        }
       </header>
+      <div className="container">
+      <Form className="form-wrap">
+        <Form.Group controlId="exampleForm.SelectCustom">
+          <Form.Control as="select" custom>
+            <option> Select a country </option>
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+          </Form.Control>
+        </Form.Group>
+      </Form>
+        <div className="result-container">
+        <Container fluid>
+        <Row>
+          <Alert variant={'warning'}>
+            'asa'
+          </Alert>
+          <Alert variant={'success'}>
+            'asa'
+          </Alert>
+          <Alert variant={'danger'}>
+            'asa'
+          </Alert>
+        </Row>
+      </Container>
+        </div>
+      </div>
     </div>
   );
 }
